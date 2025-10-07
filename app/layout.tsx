4ccb82c32df149e1a483e4c71134460b802bdb88
Rares Sandu <rares.sandu@mindera.com>
2025-10-07 15:56:12 +0300

    fix image

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,33 +12,30 @@ const theseasons = localFont({
 })
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://laura-robert.vercel.app/'),
+  metadataBase: new URL("https://laura-robert.vercel.app"),
   title: "Laura & Robert - Hai la nunta!",
   description: "Invitatie nunta Laura & Robert - 17 Mai 2025",
-  alternates: {
-    canonical: 'https://laura-robert.vercel.app/',
-  },
   openGraph: {
-    title: 'Laura & Robert - Hai la nunta!',
-    description: 'Invitatie nunta Laura & Robert - 17 Mai 2025',
+    type: "website",
+    locale: "ro_RO",
     url: "https://laura-robert.vercel.app/",
-    siteName: 'Laura & Robert - Hai la nunta!',
+    siteName: "Laura & Robert - Hai la nunta!",
     images: [
       {
-        url: 'https://laura-robert.vercel.app/opengraph-image.jpeg',
+        url: "https://laura-robert.vercel.app/opengraph-image.jpeg",
         width: 1200,
         height: 630,
-        alt: 'Laura & Robert - Hai la nunta!',
-        type: 'image/jpeg',
-      }
-    ]
+        alt: "Laura & Robert - Hai la nunta!",
+        type: "image/jpeg",
+      },
+    ],
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Laura & Robert - Hai la nunta!',
-    description: 'Invitatie nunta Laura & Robert - 17 Mai 2025',
-    images: ['https://laura-robert.vercel.app/opengraph-image.jpeg'],
-  }
+    card: "summary_large_image",
+    title: "Laura & Robert - Hai la nunta!",
+    description: "Invitatie nunta Laura & Robert - 17 Mai 2025",
+    images: ["https://laura-robert.vercel.app/opengraph-image.jpeg"],
+  },
 };
 
 export default function RootLayout({
