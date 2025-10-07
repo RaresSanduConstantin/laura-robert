ab947e57555dfe0b294b806f608358a8a668fa38
Rares Sandu <rares.sandu@mindera.com>
2025-10-07 12:41:35 +0300

    maybe fix fonts import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,15 @@
 import type { Metadata } from "next";
-import fontPreferat from "next/font/local";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const lauraFontsLight = fontPreferat({
-src: "public/fonts/Fonts-Preferate/theseasons-lt.5a8d17361a24ac511bd288ef1a.c551ff82d9639568084e4e303140e0a2.woff2",
-weight: "300",
-style: "normal",
-display: "swap",
-
-});
-
-const lauraFontsRegular = fontPreferat({
-  src: "public/fonts/Fonts-Preferate/theseasons-reg.57d3e9b5285fa8dfd595ef780.57981c5d6c03cd30856912da5d26c788.woff2",
-  weight: "400",
-  style: "normal",
-  display: "swap",
-  
-  });
+const theseasons = localFont({
+  src: [
+    {path: 'fonts/theseasons-lt.5a8d17361a24ac511bd288ef1a.c551ff82d9639568084e4e303140e0a2.woff2', weight: '300', style: 'normal'},
+    {path: 'fonts/theseasons-reg.57d3e9b5285fa8dfd595ef780.57981c5d6c03cd30856912da5d26c788.woff2', weight: '400', style: 'normal'},
+  ],
+  variable: '--font-theseasons',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   metadataBase: new URL('http://localhost:3000'),
@@ -38,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${lauraFontsLight.className} ${lauraFontsRegular.style} antialiased`}
+       className={theseasons.className}
       >
         {children}
       </body>
