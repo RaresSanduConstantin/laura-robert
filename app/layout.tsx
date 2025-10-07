f64341b2d565ce19bc7197967ae58c2b8dd748a1
Rares Sandu <rares.sandu@mindera.com>
2025-10-07 12:35:20 +0300

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import fontPreferat from "next/font/local";
 import "./globals.css";
 
 const lauraFontsLight = fontPreferat({
-src: "./fonts/Fonts-Preferate/theseasons-lt.5a8d17361a24ac511bd288ef1a.c551ff82d9639568084e4e303140e0a2.woff2",
+src: "public/fonts/Fonts-Preferate/theseasons-lt.5a8d17361a24ac511bd288ef1a.c551ff82d9639568084e4e303140e0a2.woff2",
 weight: "300",
 style: "normal",
 display: "swap",
@@ -11,7 +11,7 @@ display: "swap",
 });
 
 const lauraFontsRegular = fontPreferat({
-  src: "./fonts/Fonts-Preferate/theseasons-reg.57d3e9b5285fa8dfd595ef780.57981c5d6c03cd30856912da5d26c788.woff2",
+  src: "public/fonts/Fonts-Preferate/theseasons-reg.57d3e9b5285fa8dfd595ef780.57981c5d6c03cd30856912da5d26c788.woff2",
   weight: "400",
   style: "normal",
   display: "swap",
